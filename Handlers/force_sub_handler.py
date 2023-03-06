9703e5f4b04df877a69e70491152c813b00e8458
izakhan <121670888+izakhan@users.noreply.github.com>
2023-03-06 16:20:03 +0530

    Add files via upload

---
+++ Handlers/force_sub_handler.py
@@ -0,0 +1,54 @@
+# (c) @senuinfinity
+
+import asyncio
+from configs import Config
+from pyrogram import Client
+from pyrogram.errors import FloodWait, UserNotParticipant
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+
+async def handle_force_sub(bot: Client, cmd: Message):
+    try:
+        user = await bot.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL), user_id=cmd.from_user.id)
+        if user.status == "kicked":
+            await bot.send_message(
+                chat_id=cmd.from_user.id,
+                text="sir, සමාවෙන්න ඔයාව ban කරලා තියන්නේ . support group එකෙන් අහල බලන්න [Support Group](https://t.me/groupdcbots).",
+                parse_mode="markdown",
+                disable_web_page_preview=True
+            )
+            return 400
+    except UserNotParticipant:
+        try:
+            invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+            invite_link = await bot.create_chat_invite_link(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL))
+        except Exception as err:
+            print(f"Unable to do Force Subscribe to {Config.UPDATES_CHANNEL}\n\nError: {err}")
+            return 200
+        await bot.send_message(
+            chat_id=cmd.from_user.id,
+            text="Sorry😑 Join Our Channel To Use Me ❗",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🤖 Join Updates Channel", url=invite_link.invite_link)
+                    ],
+                    [
+                        InlineKeyboardButton("🔄 Refresh 🔄", callback_data="refreshForceSub")
+                    ]
+                ]
+            ),
+            parse_mode="markdown"
+        )
+        return 400
+    except Exception:
+        await bot.send_message(
+            chat_id=cmd.from_user.id,
+            text="Something went Wrong. Contact my [Support Group](https://t.me/groupdc).",
+            parse_mode="markdown",
+            disable_web_page_preview=True
+        )
+        return 400
+    return 200
